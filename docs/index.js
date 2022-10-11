4ad38db1ef1d7fb6459dd291343e9f31f1e9b5e8
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2022-10-11 17:46:30 -0400

    Update index.js

--- docs/index.js
+++ docs/index.js
@@ -239,7 +239,7 @@ function serialize(obj) {
         if (obj[key] === null) {
           objFlat[key] = null;
         } else if (obj[key] instanceof ArrayBuffer) {
-          objFlat[key] = Array.from(obj[key]);
+          objFlat[key] = Array.from(new Uint8Array(obj[key]));
         } else {
           objFlat[key] = serialize(obj[key]);
         }
@@ -255,7 +255,7 @@ function deserialize(text) {
 }
 
 function deserializeArrayBuffer(arr) {
-  return ArrayBuffer.from(arr);
+  return Uint8Array.from(arr).buffer;
 }
 
 function deserializeOptions(obj) {
